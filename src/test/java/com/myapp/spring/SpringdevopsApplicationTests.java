67e104c855190d66205fbd2573f8f187f49e5cb7
chennareddipalligayathri <cgayathri6171@gmail.com>
2021-03-10 11:58:34 -0800

    modified files'

--- src/test/java/com/myapp/spring/SpringdevopsApplicationTests.java
+++ src/test/java/com/myapp/spring/SpringdevopsApplicationTests.java
@@ -1,13 +1,8 @@
 package com.myapp.spring;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringdevopsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
